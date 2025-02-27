829fd18d79ca40baa9b9d4fc845700d83ca3ecc4
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 03:23:21 +0900

    [level 0] Title: 다항식 더하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -3,8 +3,6 @@ function solution(polynomial) {
     let numX = []; // x항만 있는 배열
     let numO = []; // 상수항만 있는 배열
     let numXremove = []; // x항의 x제거
-    // let numx;
-    // let numo;
     let  splitPol = polynomial.split(" + ");
     
     // 동류항끼리 분류하기
@@ -17,21 +15,19 @@ function solution(polynomial) {
     for(var i=0; i<numX.length; i++){
         numXremove.push(Number(numX[i].slice(0, -1))); //x제거
         if(numXremove[i] == "") numXremove[i] = 1;     //""일 경우 1 넣기
-        
     }
+    
     const numxSum = numXremove.reduce((acc, num) => acc + num, 0); // x항 합
     let numXSum = String(numxSum) + "x";  // 다시 x 붙이기
     if(numXSum == "1x") numXSum = "x";    // 조건에 따르면 1x는 x로 한다.
     
+    
     const numoSum = numO.reduce((acc, num) => acc + num, 0); //상수항 합
     
-    if(numoSum == 0)              // 상수항이 없을 경우
-       answer = numXSum;
-    else if(numXSum == "0x")      // x항이 없을 경우
-        answer = String(numoSum);
-    else                          // x항과 상수항 모두 있을 경우
-         answer = numXSum + " + " +  numoSum;
-        
+    
+    if(numoSum == 0) answer = numXSum;                  // 상수항이 없을 경우
+    else if(numXSum == "0x") answer = String(numoSum);  // x항이 없을 경우
+    else answer = numXSum + " + " +  numoSum;           // x항과 상수항 모두 있을 경우
     return answer;
     
 }
