a9aa9da7f11505ef7a7253aeb4b67fb5f20ead8d
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-27 17:00:44 +0900

    [level 0] Title: 분수의 덧셈, Time: 5.51 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,7 +1,7 @@
 function solution(numer1, denom1, numer2, denom2) {
-    let lcm = 1;  // 최소공배수
     
     // 최소공배수로 분모 통일하기
+    let lcm = 1;  // 최소공배수
     while(true){
         if((lcm % denom1 == 0) && (lcm % denom2 == 0)) 
             break;
@@ -14,13 +14,6 @@ function solution(numer1, denom1, numer2, denom2) {
     let numerHap = firstNumer + SecondNumer; // 분자의 합
    
     // 최대공약수 구하기
-    // let gcd = 1;
-    // let getGCD = (numerHap, lcm) => {
-    //     for(let i=2; i<= Math.min(numerHap, lcm); i++){
-    //         if(numerHap % i === 0 && lcm % i === 0) 
-    //             gcd = i;
-    //     }
-    // };
     const getGCD = (a, b)  => a % b === 0 ? b : getGCD(b, a%b);
     const gcd = getGCD(numerHap, lcm);
     
