0ad077fe4198b8e4ca66ff58e3311b9b231c9092
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 03:21:08 +0900

    [level 0] Title: 다항식 더하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,40 +1,35 @@
 function solution(polynomial) {
     var answer = '';
-    let numX = [];
-    let numO = [];
-    let numx;
-    let numo;
-    let numXremove = [];
+    let numX = []; // x항만 있는 배열
+    let numO = []; // 상수항만 있는 배열
+    let numXremove = []; // x항의 x제거
+    // let numx;
+    // let numo;
     let  splitPol = polynomial.split(" + ");
     
     // 동류항끼리 분류하기
     for(var i=0; i<splitPol.length; i++){
-        if(splitPol[i].indexOf("x") == -1){
-            numO.push(Number(splitPol[i]));  // 상수항만
-        }
-        else{
-            numX.push(splitPol[i]); // x항만
-        }
+        if(splitPol[i].indexOf("x") == -1) numO.push(Number(splitPol[i]));  // 상수항만
+        else numX.push(splitPol[i]);  // x항만
     }
     
     // 차수의 x 제거 
     for(var i=0; i<numX.length; i++){
         numXremove.push(Number(numX[i].slice(0, -1))); //x제거
-        if(numXremove[i] == "")//""일 경우 1 넣기
-            numXremove[i] = 1; 
+        if(numXremove[i] == "") numXremove[i] = 1;     //""일 경우 1 넣기
         
     }
-    const numxSum = numXremove.reduce((acc, num) => acc + num, 0); // x차수항 합
-    let numXSum = String(numxSum) + "x";  // 다시 x붙이기
-    if(numXSum == "1x") numXSum = "x";
+    const numxSum = numXremove.reduce((acc, num) => acc + num, 0); // x항 합
+    let numXSum = String(numxSum) + "x";  // 다시 x 붙이기
+    if(numXSum == "1x") numXSum = "x";    // 조건에 따르면 1x는 x로 한다.
     
     const numoSum = numO.reduce((acc, num) => acc + num, 0); //상수항 합
     
-    if(numoSum == 0)
+    if(numoSum == 0)              // 상수항이 없을 경우
        answer = numXSum;
-    else if(numXSum == "0x")
+    else if(numXSum == "0x")      // x항이 없을 경우
         answer = String(numoSum);
-    else
+    else                          // x항과 상수항 모두 있을 경우
          answer = numXSum + " + " +  numoSum;
         
     return answer;
