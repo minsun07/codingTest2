bd6dac8ccbd22bbcb8b2268f00ddc69c19f0bd03
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-27 15:25:55 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,7 +1,7 @@
-function solution(numbers) {
-    var answer = 0;
-    for(let i=0; i<numbers.length; i++){
-        answer += numbers[i];
-    }
-    return answer/numbers.length;
+function solution(numbers) {    
+    return numbers.reduce((acc, cur) => acc + cur, 0) / numbers.length;
+    
+    //reduce란 각 요소의 합산한 결과를 하나의 값으로 리턴할 때 많이 사용된다.
+    // acc는 누적값, cur은 현재 처리중인 값
+    // 0은 초기값을 말한다. 만약 3으로 한다면 acc의 초기값이 3이 되는것이다.
 }
