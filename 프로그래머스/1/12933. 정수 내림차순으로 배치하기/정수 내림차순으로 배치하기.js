26e5aaddff49a5c9a8ed3fc0604d33ea5d55b266
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 22:33:26 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let strN = String(n);
     let arrStrN = strN.split("");
-    let sortStrN = arrStrN.sort().reverse();
+    let sortStrN = arrStrN.sort().reverse(); // 오름차순 정렬 후, 뒤집기
     let strSum = sortStrN.reduce((a, b) => a+b);
     
     return Number(strSum);
