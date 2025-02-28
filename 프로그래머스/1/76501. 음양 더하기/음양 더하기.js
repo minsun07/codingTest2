9fbe8c0630c954ba7fc417de53b2f1a7d7c62da2
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 23:16:30 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,11 @@
 function solution(absolutes, signs) {
-    let sum = 0;
-    for(let i=0; i<signs.length; i++){
-        if(signs[i] == true) sum += absolutes[i];   
-        else sum -= absolutes[i]; 
-    }
-    return sum;
+    // let sum = 0;
+    // for(let i=0; i<signs.length; i++){
+    //     if(signs[i] == true) sum += absolutes[i];   
+    //     else sum -= absolutes[i]; 
+    // }
+    // return sum;
+    
+    // 다른 간결한 방법
+    return absolutes.reduce((acc, val, i) => acc + val * (signs[i] ? 1 : -1), 0)
 }
