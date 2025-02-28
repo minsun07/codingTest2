364acb9c613bc3d28ef3ba37169da4685e6e79b4
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 23:00:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,10 +1,9 @@
 function solution(n) {
-    var sqrtNum = Math.sqrt(n);
-    if(sqrtNum % 1 == 0){
-        return (sqrtNum+1)**2;
+    var sqrtNum = Math.sqrt(n); // 제곱근 구하기
+    if(sqrtNum % 1 == 0){       // 정수, 실수 구분
+        return (sqrtNum+1)**2;  // 제곱 구하기
     }
     else{
         return -1;
     }
-    // return 11 % 2;
 }
