2de6279aec621a827458cda1e620a46514a5b119
안민선 <142420604+minsun07@users.noreply.github.com>
2025-03-01 01:15:37 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -2,7 +2,7 @@ function solution(s) {
     let arrS = [];
     if(s.length == 4 || s.length == 6){
         arrS = s.split("");
-        return arrS.every(item => !isNaN(item));
+        return arrS.every(item => !isNaN(item)); // every()는 배열의 모든 요소가 숫자인자 확인해주는 함수이다. 있을 경우 false반환.
     } 
     return false;
 }
