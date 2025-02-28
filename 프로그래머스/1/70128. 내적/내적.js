06ae3c811e41d0629ce860407fb4293e1db57db2
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 12:08:56 +0900

    [level 1] Title: 내적, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,12 @@
 function solution(a, b) {
-    let sum = 0;
-    for(let i=0; i<a.length; i++){
-        sum += a[i]*b[i];
-    }
-    return sum;
+    // let sum = 0;
+    // for(let i=0; i<a.length; i++){
+    //     sum += a[i]*b[i];
+    // }
+    // return sum;
+    
+    // 더 간결한 풀이
+    return a.reduce((acc, _, i) => acc += a[i] * b[i],0);
+    // acc : 누적된 합
+    // _ : (acc, currentValue, index, array)여기에서 currentValue(현재 순회 중인 배열의 요소)는 사용하지 않으므로 
 }
