c3593758dc2155f9ade62913b7f2bf95c5382a8f
안민선 <142420604+minsun07@users.noreply.github.com>
2025-02-28 22:50:56 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,9 @@
 function solution(phone_number) {
-    let arrpn = phone_number.split("");
+    let arrpn = phone_number.split(""); // 각 자리를 배열에 넣기
     
-    for(let i=0; i<arrpn.length-4; i++){
-        arrpn[i] = "*";
+    for(let i=0; i<arrpn.length-4; i++){ // 뒤 4자리 뺀 배열 반복
+        arrpn[i] = "*"; // *로 치환
     }
-    let answer = arrpn.reduce((a,b) => a+b);
+    let answer = arrpn.reduce((a,b) => a+b); // 배열 합치기 
     return answer;
 }
