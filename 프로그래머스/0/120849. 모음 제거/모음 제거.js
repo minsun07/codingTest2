77c23687b319e783e7a555c412d13be38cd12690
안민선 <142420604+minsun07@users.noreply.github.com>
2025-03-02 02:59:41 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,10 +1,11 @@
 function solution(my_string) {
-    my_string = my_string.replace(/a/g, "")
-                         .replace(/e/g, "")
-                         .replace(/i/g, "")
-                         .replace(/o/g, "")
-                         .replace(/u/g, "");
+    // my_string = my_string.replace(/a/g, "")
+    //                      .replace(/e/g, "")
+    //                      .replace(/i/g, "")
+    //                      .replace(/o/g, "")
+    //                      .replace(/u/g, "");
+    // return my_string;
     
-    
-    return my_string;
+    // 간단한 코드
+    return my_string.replace(/[aeiou]/g, "");
 }
